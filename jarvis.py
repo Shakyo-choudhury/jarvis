9e72e0298b7c4a5f9be9478d13627773586940e5
Shakyo choudhury <94360466+Shakyo-choudhury@users.noreply.github.com>
2023-12-08 16:02:42 +0530

    Add files via upload

---
+++ jarvis.py
@@ -0,0 +1,80 @@
+import pyttsx3 
+import speech_recognition as sr
+import datetime 
+import wikipedia 
+import webbrowser
+
+# pyttsx3.speak('hello shakyo')
+
+engine = pyttsx3.init('sapi5')
+voices = engine.getProperty('voices')
+# print(voices[0].id)
+engine.setProperty('voice', voices[0].id)
+
+def speak(audio):
+    engine.say(audio)
+    engine.runAndWait()
+
+def wishMe():
+    hour = int(datetime.datetime.now().hour)
+
+    if hour >= 0 and hour <12:
+        speak('good morning my master')
+    
+    elif hour >= 12 and hour<18:
+        speak('good afternoon my master')
+    
+    else:
+        speak('good evening my master')
+    
+    speak('hey there sir, i am jarvis how can i help you')
+
+
+def takeCommand():
+    # it takes the command as a speech from the user and converts it into a text which can be understood by the computer
+
+    r = sr.Recognizer()
+    with sr.Microphone() as source:
+        print('computing...')
+        r.pause_threshold = 1
+        audio = r.listen(source)
+
+    try:
+        print('recognizing...')
+        query = r.recognize_google(audio, language = 'en-in')
+        print(f'user said: \n{query}')
+    
+    except Exception as e:
+        print('say that again sir...')
+        return "None"
+    return query
+
+
+if __name__ == '__main__':
+    wishMe()
+    while True:
+        query = takeCommand().lower()
+        
+        if 'wikipedia' in query:
+            speak('searching for wikipedia...')
+            query = query.replace('wikipedia'," ") 
+            results = wikipedia.summary(query, sentences = 4)
+            speak('according to wikipedia')
+            print(results)
+            speak(results)
+
+        elif 'open youtube' in query:
+            speak('searching for youtube...')
+            webbrowser.open('http://www.youtube.com/watch')
+
+        elif 'open google' in query:
+            speak('searching for google...')
+            webbrowser.open('http://www.google.com')
+        elif 'open stack overflow' in query:
+            speak('searching for stack overflow...')
+            webbrowser.open('https://stackoverflow.com/')
+
+        else:
+            speak('these are the end of my capabilities, master...')
+
+
